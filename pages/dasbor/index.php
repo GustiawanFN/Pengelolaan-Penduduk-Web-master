2f09e73a93a4e0cac3d298dde54e2ad965502dc9
fajarnandagusti <fnandagustiawan@gmail.com>
2019-12-28 04:57:06 +0700

    rev dashboard

--- pages/dasbor/index.php
+++ pages/dasbor/index.php
@@ -1,23 +1,43 @@
-
 <?php include('../mode/top.php') ?>
 
 <!-- page content -->
-        <div class="right_col" role="main">
-          
+<div class="right_col" role="main">
+  <div class="">
+    <div class="page-title">
+      <div class="title_left">
+        <br>
 
-                  </div>
-                </div>
+      </div>
+      <div class="clearfix"></div>
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+          <div class="x_panel">
+            <div class="x_title">
+              <h1 align="center">Selamat Datang Di Website Pengelolaan Penduduk Desa Cipetir</h1>
+
+              <!-- <ul class="nav navbar-right panel_toolbox">
+                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                </li>
+              </ul> -->
+
+              <div class="clearfix">
+
               </div>
+            </div>
+            <div class="x_content">
 
-              
+              <img class="x_content center-fit" width="1033px" src="../../img/bale.png">
 
-              
 
-            
             </div>
           </div>
         </div>
-        <!-- /page content -->
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- /page content -->
 
 
 <?php include('../mode/bottom.php') ?>
